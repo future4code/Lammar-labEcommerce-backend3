0827f5860f479317f5b092e3cec959ed7c13138c
Arissa Yasmin Iwashiro Otsu <arissaotsu@gmail.com>
2022-12-01 22:25:30 -0300

    Correção

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from 'cors'
+import dotenv from "dotenv"
+dotenv.config();
 
 export const app = express()
 
@@ -7,6 +9,6 @@ app.use(express.json())
 
 app.use(cors())
 
-app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003");
+app.listen(process.env.PORT|| 3003, () => {
+    console.log(`Server is running in ${process.env.PORT}`);
 });
